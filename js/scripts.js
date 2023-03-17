ddaee68f180c7dc7c4c069e41a5fdff614a1f1ab
PedroPenha <pedro.eng98@gmail.com>
2023-03-17 16:48:39 -0300

    navegation_js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 const navTabs = document.querySelectorAll('.js-nav-tabs li a');
 const tabPane = document.querySelectorAll('.js-tab-pane');
 
-navTabs.forEach(nav => {
+//Transition Btns
+navTabs.forEach((nav, index) => {
     nav.addEventListener('click', (event) =>{
         event.preventDefault();
 
+//Percorre e remove a classe ativa de todos 
         navTabs.forEach(itemNav => {
             itemNav.classList.remove('active')
         })
+        tabPane.forEach(tab => {
+            tab.classList.remove('active')
+        })
 
+//Aciona a classe ativa no elemento selecionado        
         nav.classList.add('active');
+        tabPane[index].classList.add('active');
     })
 })
-
-
-
-
-
-console.log(navTabs);
